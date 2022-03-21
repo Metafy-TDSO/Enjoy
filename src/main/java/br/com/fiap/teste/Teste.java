21e6eab7f95a301a0d29666553c5ed0601974eb1
Guilherme Vieira <guivpw68@gmail.com>
2022-03-21 17:18:38 -0300

    style: formatar código

--- src/main/java/br/com/fiap/teste/Teste.java
+++ src/main/java/br/com/fiap/teste/Teste.java
@@ -1,5 +1,7 @@
 package br.com.fiap.teste;
 
+import br.com.fiap.entities.Comanda;
+import br.com.fiap.entities.Consumidor;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -8,84 +10,79 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.fiap.entities.Comanda;
-import br.com.fiap.entities.Consumidor;
-
 public class Teste {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
 
-		Scanner leitor = new Scanner(System.in);
+    EntityManagerFactory fabrica = Persistence.createEntityManagerFactory(
+      "enjoy"
+    );
+    EntityManager em = fabrica.createEntityManager();
 
-		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("enjoy");
-		EntityManager em = fabrica.createEntityManager();
+    System.out.println("Digite o telefone: ");
+    String telefone = leitor.next() + leitor.nextLine();
 
-		System.out.println("Digite o telefone: ");
-		String telefone = leitor.next() + leitor.nextLine();
-		
-		Consumidor consumidor = em.createQuery(
-				  "SELECT u from Consumidor u WHERE u.telefone = :telefone", Consumidor.class).
-				  setParameter("telefone", telefone).getSingleResult();
+    Consumidor consumidor = em
+      .createQuery(
+        "SELECT u from Consumidor u WHERE u.telefone = :telefone",
+        Consumidor.class
+      )
+      .setParameter("telefone", telefone)
+      .getSingleResult();
 
-		System.out.println(consumidor);
+    System.out.println(consumidor);
 
-		List<Comanda> comandas = consumidor.getComandas();		
-		Date maiorData = comandas.get(0).getDataEntrada();		
-				
-		List<String> datasEntrada = new ArrayList<String>();
-		
-		double somaValor = 0;
-		
-		for (Comanda comanda : comandas) {
-			
-			if(maiorData.compareTo(comanda.getDataEntrada()) == -1) {
-				maiorData = comanda.getDataEntrada();
-				
-			}
-			
-			Calendar calendar = Calendar.getInstance();
-			calendar.setTime(comanda.getDataEntrada());
-			int month = calendar.get(Calendar.MONTH)+1;
-			int year = calendar.get(Calendar.YEAR);
-			
-			datasEntrada.add(Integer.toString(month) + "/" + Integer.toString(year) );			
-			
-			
-			somaValor = somaValor + comanda.getValorComanda();
-			
-		}	
-		
-		Set<String> distinct = new HashSet<String>(datasEntrada);
-		
-		double soma = 0;
-		double contadorMeses = 0;
-		
-        for (String s: distinct) {
-        	soma = soma + Collections.frequency(datasEntrada, s);
-        	contadorMeses = contadorMeses +1;
-        }
-		
-		double frequencia; 
-		
-		frequencia =  soma/contadorMeses;	
-		
-		double mediaValores = somaValor/comandas.toArray().length;
-				
-		System.out.println("A data da ultima visita eh: " + maiorData);				
-		
-		System.out.println("A frequencia mensal eh: " + frequencia);
-		
-		System.out.println("A media de valores eh: " + mediaValores);
+    List<Comanda> comandas = consumidor.getComandas();
+    Date maiorData = comandas.get(0).getDataEntrada();
 
-		
-		em.close();
-		fabrica.close();
+    List<String> datasEntrada = new ArrayList<String>();
 
-	}
+    double somaValor = 0;
 
+    for (Comanda comanda : comandas) {
+      if (maiorData.compareTo(comanda.getDataEntrada()) == -1) {
+        maiorData = comanda.getDataEntrada();
+      }
+
+      Calendar calendar = Calendar.getInstance();
+
+      calendar.setTime(comanda.getDataEntrada());
+      int month = calendar.get(Calendar.MONTH) + 1;
+      int year = calendar.get(Calendar.YEAR);
+
+      datasEntrada.add(Integer.toString(month) + "/" + Integer.toString(year));
+      somaValor = somaValor + comanda.getValorComanda();
+    }
+
+    Set<String> distinct = new HashSet<String>(datasEntrada);
+
+    double soma = 0;
+    double contadorMeses = 0;
+
+    for (String s : distinct) {
+      soma = soma + Collections.frequency(datasEntrada, s);
+      contadorMeses = contadorMeses + 1;
+    }
+
+    double frequencia;
+
+    frequencia = soma / contadorMeses;
+
+    double mediaValores = somaValor / comandas.toArray().length;
+
+    System.out.println("A data da ultima visita eh: " + maiorData);
+
+    System.out.println("A frequencia mensal eh: " + frequencia);
+
+    System.out.println("A media de valores eh: " + mediaValores);
+
+    em.close();
+    fabrica.close();
+    leitor.close();
+  }
 }
