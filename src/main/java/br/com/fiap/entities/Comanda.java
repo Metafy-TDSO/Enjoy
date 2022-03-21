f32ef95773b5b6104383d9948462b9c7f35c304f
Guilherme Vieira <guivpw68@gmail.com>
2022-03-21 17:12:10 -0300

    feat: adicionar index e adicionar propriedades da coluna

--- src/main/java/br/com/fiap/entities/Comanda.java
+++ src/main/java/br/com/fiap/entities/Comanda.java
@@ -1,6 +1,5 @@
 package br.com.fiap.entities;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -11,6 +10,7 @@ import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -19,107 +19,106 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "tb_comanda")
+@Table(name = "tb_comanda", indexes = @Index(columnList = "dt_entrada", name = "idx_dt_entrada DESC"))
 public class Comanda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_comanda")
+    private int id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_comanda")
-	private int id;
+    @Column(name = "dt_entrada", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataEntrada;
 
-	@Column(name = "dt_entrada")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date dataEntrada;
+    @Column(name = "vl_valor", scale = 2, precision = 10)
+    private double valorComanda;
 
-	@Column(name = "vl_valor")
-	private double valorComanda;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "id_consumidor", foreignKey = @ForeignKey(name = "fk_comanda_consumidor"), nullable = false)
+    private Consumidor consumidor;
 
-	@ManyToOne(cascade = CascadeType.PERSIST)
-	@JoinColumn(name = "id_consumidor", foreignKey = @ForeignKey(name = "fk_comanda_consumidor"), nullable = false)
-	private Consumidor consumidor;
+    @OneToMany(mappedBy = "comanda")
+    private List<Pedido> pedidos;
 
-	@OneToMany(mappedBy = "comanda")
-	private List<Pedido> pedidos;
+    public Comanda() {
 
-	public Comanda() {
+    }
 
-	}
+    public Comanda(int id, Date dataEntrada, double valorComanda, Consumidor consumidor, List<Pedido> pedidos) {
+	super();
+	this.id = id;
+	this.dataEntrada = dataEntrada;
+	this.valorComanda = valorComanda;
+	this.consumidor = consumidor;
+	this.pedidos = pedidos;
+    }
 
-	public Comanda(int id, Date dataEntrada, double valorComanda, Consumidor consumidor, List<Pedido> pedidos) {
-		super();
-		this.id = id;
-		this.dataEntrada = dataEntrada;
-		this.valorComanda = valorComanda;
-		this.consumidor = consumidor;
-		this.pedidos = pedidos;
-	}
+    public int getId() {
+	return id;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public Date getDataEntrada() {
+	return dataEntrada;
+    }
 
-	public Date getDataEntrada() {
-		return dataEntrada;
-	}
+    public void setDataEntrada(Date dataEntrada) {
+	this.dataEntrada = dataEntrada;
+    }
 
-	public void setDataEntrada(Date dataEntrada) {
-		this.dataEntrada = dataEntrada;
-	}
+    public double getValorComanda() {
+	return valorComanda;
+    }
 
-	public double getValorComanda() {
-		return valorComanda;
-	}
+    public void setValorComanda(double valorComanda) {
+	this.valorComanda = valorComanda;
+    }
 
-	public void setValorComanda(double valorComanda) {
-		this.valorComanda = valorComanda;
-	}
+    public Consumidor getConsumidor() {
+	return consumidor;
+    }
 
-	public Consumidor getConsumidor() {
-		return consumidor;
-	}
+    public void setConsumidor(Consumidor consumidor) {
+	this.consumidor = consumidor;
+    }
 
-	public void setConsumidor(Consumidor consumidor) {
-		this.consumidor = consumidor;
-	}
+    public List<Pedido> getPedidos() {
+	return pedidos;
+    }
 
-	public List<Pedido> getPedidos() {
-		return pedidos;
-	}
+    public void setPedidos(List<Pedido> pedidos) {
+	this.pedidos = pedidos;
+    }
 
-	public void setPedidos(List<Pedido> pedidos) {
-		this.pedidos = pedidos;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	return result;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Comanda other = (Comanda) obj;
+	if (id != other.id)
+	    return false;
+	return true;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comanda other = (Comanda) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Comanda [id=" + id + ", dataEntrada=" + dataEntrada + ", valorComanda=" + valorComanda + ", consumidor="
-				+ consumidor + "]";
-	}
+    @Override
+    public String toString() {
+	return "Comanda [id=" + id + ", dataEntrada=" + dataEntrada + ", valorComanda=" + valorComanda + ", consumidor="
+		+ consumidor + "]";
+    }
 
 }
